433318a7c6a96374db26d373e6cf5251ea83a265
Guillermo <guillermo.mendez@kairosds.com>
2018-07-04 16:40:04 +0200

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Manolo' },
+  { id: 12, name: 'Restituto' },
+  { id: 13, name: 'Claudio' },
+  { id: 14, name: 'Paco' },
+  { id: 15, name: 'Fulgencia' },
+  { id: 16, name: 'Pepa' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
